84d82bb1303224f3acfd4df588e68e873ad1b7bb
David Viloria <david.viloria0424@gmail.com>
2021-11-11 22:36:39 -0500

    Nuevo archivo Category
    Se añade el archivo category usando el protocolo Codable

---
+++ Trivia/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  Trivia
+//
+//  Created by David Viloria Ortega on 11/11/21.
+//
+
+import Foundation
+
+struct Category: Codable {
+    var id: Int
+    var name: String
+}
