70c276818d7c3826e63eb2c3ea49a9e558cf1140
David Viloria <david.viloria0424@gmail.com>
2021-11-11 23:13:07 -0500

    Logica agregada

--- Trivia/Questions.swift
+++ Trivia/Questions.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+
+struct Question: Codable {
+    var category: String
+    var type: String
+    var difficulty: String
+    var question: String
+    var correct_answer: String
+    var incorrect_answers: [String]
+}
+
+//The following code is used for testing when not fetching the questions from the API
+
+struct LocalQuestion: Codable {
+    var question: String
+    var answer: Bool
+}
+
+final class Contenido {
+    static let shared = Contenido()
+
+    private init() {}
+
+    func getQuestions() -> [LocalQuestion] {
+        return [
+            LocalQuestion(question: "Pregunta 1", answer: false),
+            LocalQuestion(question: "Pregunta 2", answer: true),
+            LocalQuestion(question: "Pregunta 3", answer: true),
+            LocalQuestion(question: "Pregunta 4", answer: false),
+            LocalQuestion(question: "Pregunta 5", answer: false),
+            LocalQuestion(question: "Pregunta 6", answer: true),
+            LocalQuestion(question: "Pregunta 7", answer: false),
+            LocalQuestion(question: "Pregunta 8", answer: true),
+            LocalQuestion(question: "Pregunta 9", answer: true),
+            LocalQuestion(question: "Pregunta 10", answer: false),
+            LocalQuestion(question: "Pregunta 11", answer: true)
+        ]
+    }
+
+}
